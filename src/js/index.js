2f6dcfd1512fb0aaef37be881102451ea9026139
ZelikSV <zeliksv@gmail.com>
2019-02-12 14:44:55 +0200

    dev, change bootstrap method

--- src/js/index.js
+++ src/js/index.js
@@ -1,41 +1,50 @@
+
 (function() {
   const directives = [{
-    name: 'ng_click',
-    function(e, cb) {
+    name: 'ng-click',
+    func(e, cb) {
       e.addEventListener('click', function() {
         const fn = new Function(cb);
         fn();
       });
     }
-  },
-  {
-    name: 'ng-module',
-    functions: []
   }
   ];
   const smallAngular = {
     directive(name, cb) {
-      directives.push({ name, functions: cb });
+      directives.push({ name, func: cb });
     },
     compile(node) {
       directives.forEach(item => {
         for (let i = 0; i < node.attributes.length; i++) {
           if (node.attributes[i].name === item.name) {
-            item.function(node, node.attributes[i].value);
+            item.func(node, node.attributes[i].value);
           }
         }
       });
     },
-    bootstrap(node) {
-      if (!node) {
-        const appWrapper = document.querySelector('[ng-app]');
-        const child = appWrapper.children;
+    bootstrap(node = '[ng-app]') {
+      const appWrapper = document.querySelector(node);
+      const child = appWrapper.children;
 
-        [...child].forEach(item => this.compile(item));
-      }
-      [...node.children].forEach(item => this.compile(item));
+      [...child].forEach(item => this.compile(item));
     }
   };
 
   window.smallAngular = smallAngular;
 }());
+
+smallAngular.directive('ng-show', function(el) {
+  console.log('called directive ng-show on element', el);
+});
+smallAngular.directive('ng-module', function(el) {
+  console.log('called directive ng-show on element', el);
+});
+smallAngular.directive('ng-make-short', function(el) {
+  console.log('called directive ng-show on element', el);
+});
+smallAngular.directive('ng-bind', function(el) {
+  console.log('called directive ng-show on element', el);
+});
+
+smallAngular.bootstrap();
